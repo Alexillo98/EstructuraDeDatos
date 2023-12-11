919dfbc5df29e9600cbcff06980fd922c46655bd
Alexillo98 <musateduardalexandru@gmail.com>
2023-12-11 08:52:09 +0100

    PaisCapital

---
+++ src/PaisCapital.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class PaisCapital
+{
+    public static void main(String[] args)
+    {
+        Map<String, String> paisCapital = new HashMap<>();
+        paisCapital.put("España","Madrid");
+        paisCapital.put("Portugal","Lisboa");
+        paisCapital.put("Alemania","Berlin");
+        paisCapital.put("Inglaterra","Londres");
+
+        Scanner escaner = new Scanner(System.in);
+        System.out.println("Introduzca el país:");
+        String pais = escaner.nextLine();
+        if(paisCapital.containsKey(pais))
+        {
+            System.out.println("La capital de " + pais + " es " + paisCapital.get(pais));
+        }else
+        {
+            System.out.println("El pais no esta en el diccionario.");
+        }
+
+    }
+}
