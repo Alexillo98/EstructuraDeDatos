135dd40697edf759b090a6f2db3af6c27afd3dfc
Alexillo98 <musateduardalexandru@gmail.com>
2023-12-14 10:42:48 +0100

    Anagramas

--- src/Anagramas.java
+++ src/Anagramas.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Anagramas
 {
@@ -12,18 +9,32 @@ public class Anagramas
     {
         BufferedReader reader = new BufferedReader(new FileReader("files/spanish-dict.txt"));
         String line;
-        Map<String, ArrayList<String>> anagrama = new HashMap<>();
-        String[] lasPalabras = {};
+        Map<String,List<String>> anagramas = new HashMap<>();
+        List<String> palabras = new ArrayList<>();
+
         while ((line = reader.readLine()) != null)
         {
-            lasPalabras = new String[line.length()];
+            String alfabetizada = alfabetizar(line);
+            palabras = anagramas.get(alfabetizada);
 
-            for(int i = 0 ; i < lasPalabras.length; i++)
+            if(palabras == null)
             {
-                Arrays.sort(lasPalabras);
+                palabras = new ArrayList<>();
+                palabras.add(line);
+                anagramas.put(line, palabras);
+
+            }else
+            {
+
             }
 
+            System.out.println(anagramas);
         }
-        System.out.println(Arrays.toString(lasPalabras));
+        reader.close();
+    }
+    public static String alfabetizar (String palabra) {
+        char[] a = palabra.toCharArray();
+        Arrays.sort(a);
+        return new String(a);
     }
 }
