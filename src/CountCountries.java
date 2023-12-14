267d96c936100770d1b878dce3c072f574cfddf8
Alexillo98 <musateduardalexandru@gmail.com>
2023-12-14 10:43:11 +0100

    Contar Paises

---
+++ src/CountCountries.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountCountries
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new FileReader("files/Colfuturo-Seleccionados.csv"));
+        String line;
+        String[] columnasSeparadas;
+        Map<String,Integer> pertenecen = new HashMap<>();
+
+        while ((line = reader.readLine()) != null)
+        {
+            reader.readLine();
+            columnasSeparadas = line.split(",");
+
+            for (int i = 0 ; i < columnasSeparadas.length ; i++)
+            {
+                if (pertenecen.containsKey(columnasSeparadas[6]))
+                {
+                    int contador = pertenecen.get(columnasSeparadas[6] + 1);
+                    pertenecen.replace(columnasSeparadas[i],contador);
+                }else
+                {
+                pertenecen.put(columnasSeparadas[6],pertenecen.getOrDefault(columnasSeparadas[6],1));
+                }
+            }
+            System.out.println(pertenecen);
+        }
+        reader.close();
+    }
+}
