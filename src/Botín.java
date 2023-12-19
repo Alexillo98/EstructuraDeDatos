e415960c3d2f373f6bc53eb8e5a019f3502b366e
Alexillo98 <musateduardalexandru@gmail.com>
2023-12-19 10:43:09 +0100

    Botin

---
+++ src/Botín.java
@@ -0,0 +1,13 @@
+public class Botín
+{
+    public static String repartir (int participantes, int billetes, String participantesBilletes)
+    {
+        String[] participatesBilletes = participantesBilletes.split(",");
+        participantes = Integer.parseInt(participantesBilletes[0]);
+        return "";
+    }
+    public static void main(String[] args)
+    {
+        String participantesBilletes = "2,10,20,50,200,500";
+    }
+}
