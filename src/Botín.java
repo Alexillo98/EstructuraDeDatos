45e9d524a7760942d666735f6b7e2f5aa85036dd
Alexillo98 <149682501+Alexillo98@users.noreply.github.com>
2024-01-10 21:15:40 +0100

    Update Botín.java

--- src/Botín.java
+++ src/Botín.java
@@ -1,14 +1,53 @@
-public class Botín
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Botin
 {
-    public static String repartir (int participantes, int billetes, String participantesBilletes)
+    //Hasta esto he llegado. No se resolverlo del todo.
+    public static int participantes (String participantesBilletes)
     {
-        String[] participatesBilletes = participantesBilletes.split(",");
-        participantes = Integer.parseInt(participantesBilletes[0]);
-        return "";
+        String[] separadoParticipantes = participantesBilletes.split(",");
+        int participantes = Integer.parseInt(separadoParticipantes[0]);
+        return participantes;
     }
+    public static ArrayList<Integer> billetes (String participantesBilletes)
+    {
+        String[] separadoBilletes = participantesBilletes.split(",");
+        int[] total = new int[separadoBilletes.length];
+        ArrayList<Integer> billetes = new ArrayList<>();
+
+        for (int i = 0 ; i < separadoBilletes.length ; i++)
+        {
+            total[i] = Integer.parseInt(separadoBilletes[i]);
+        }
+
+        for (int i = 1 ; i < total.length ; i++)
+        {
+            billetes.add(i-1,total[i]);
+        }
+        return billetes;
+    }
+    public static String repartir (int participantes, ArrayList<Integer> billetes)
+    {
+        Map<Integer,Integer> mapa1 = new HashMap<>();
+
+        for (int i = 0 ; i < participantes ; i++)
+        {
+            mapa1.put(i,0);
+        }
+
+        for (int i = 0 ; i < participantes ; i++)
+        {
+            mapa1.put(i, billetes.get(i));
+        }
+        return mapa1.toString();
+    }
+
     public static void main(String[] args)
     {
-        //MAL. Falta terminar
-        String participantesBilletes = "2,10,20,50,200,500";
+        String participantesBilletes = "3,10,20,50,200,500";
+
+        System.out.println(repartir(participantes(participantesBilletes),billetes(participantesBilletes)));
     }
 }
